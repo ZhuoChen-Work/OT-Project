fa746842301c946372aa4e00acfcfb8f00dd19b1
ZhuoChen-Work <118645613+ZhuoChen-Work@users.noreply.github.com>
2022-11-27 15:33:31 +0100

    Update brenier.py

--- brenier.py
+++ brenier.py
@@ -9,12 +9,9 @@ device = torch.device('cuda:0') if torch.cuda.is_available() else 'cpu'
 
 
 class ICNN(nn.Module):
-  """ 
-  Input convex neural network:   
-  """
-  
   def __init__(self,input_dim, dim_hidden=[100,100,100], device=device):
    """ 
+   Input convex neural network:  
    w_zs and w_xs are the weights in ICNN model, where w_zs should be positive during the training
    the number of units in each layer of ICNN is (input_dim, 1 ,dim_hidden[0],dim_hidden[1],...,dim_hidden[-1], 1 );  
 
@@ -33,7 +30,6 @@ class ICNN(nn.Module):
     self.input_dim = input_dim
     self.num_hidden = len(dim_hidden)
     self.dim_hidden = dim_hidden
-
 
     self.w_zs = nn.ModuleList()
     for i in range(self.num_hidden):
@@ -79,13 +75,14 @@ class ICNN(nn.Module):
 
 
 class LSE(nn.Module):
+  def __init__(self,input_dim,hid_dim=200,device=device,alpha=0.5):
   """
   Log Sum Exp 
   """
-  def __init__(self,input_dim,hid_dim=200,device=device,alpha=0.5):
     super(LSE, self).__init__()
     self.W = nn.Linear(input_dim,hid_dim,bias=True)
     self.alpha = alpha
+    
   def forward(self,x):
   """
   The forward function consist of two parts: 1) the standard LSE function; 2) alpha*||x||^2
@@ -98,20 +95,18 @@ class LSE(nn.Module):
   """
   According to Brenier theorem, the optimal transport mapping is the gradiant of the convex forward function
   """
-
     temp = torch.clone(x)
     temp.requires_grad_()
     loss=self.forward(temp)
     loss.backward(torch.ones_like(loss))
-
     return temp.grad
 
 class c_transform(nn.Module):
+  def __init__(self,train_target,convex_func):
   """
   C-transform 
   https://arxiv.org/pdf/1910.03875.pdf,  formula(20),(21)
   """
-  def __init__(self,train_target,convex_func):
     super(c_transform, self).__init__()
     self.target = train_target
     self.init_target = train_target
@@ -158,6 +153,9 @@ class c_transform(nn.Module):
 
 class MLP(nn.Module):
   def __init__(self,input_dim,dim_hidden=[100,100,100]):
+    """
+    Standard  MLP model
+    """
     super(MLP,self).__init__()
     self.input_dim = input_dim
     self.num_hidden = len(dim_hidden)
@@ -186,7 +184,7 @@ class MLP(nn.Module):
 
 def LSE_train(lse_model,device, train_loader,optimizer, epoch ,print_bool=True):
     """
-    lse C-Transform model
+    Training lse C-Transform model, loss is the wasserstein dual form
     """
     model = lse_model.to(device)
     for ep in range(epoch):
@@ -204,7 +202,7 @@ def LSE_train(lse_model,device, train_loader,optimizer, epoch ,print_bool=True):
 
 def ICNN_train(model_icnn,device, train_loader,optimizer, epoch,print_bool=True):
     """
-    ICNN C-Transform model
+    Training ICNN C-Transform model, loss is the wasserstein dual form
     """
     model = model_icnn.to(device)
     for ep in range(epoch):
@@ -225,7 +223,7 @@ def ICNN_train(model_icnn,device, train_loader,optimizer, epoch,print_bool=True)
 
 def MLP_train(model_mlp,device, train_loader, target, optimizer, epoch,print_bool=False):
     """
-    loss is the sum of squared error.
+    Training MLP moldel, loss is the sum of squared error.
     """
     model = model_mlp.to(device)
 
@@ -271,35 +269,34 @@ def get_wdist(source,target,device=device):
 
 
 def mix_train( model_icnn,device, train_loader, optimizer, epoch,alpha=0,print_bool=True):
-    """
-    hybrid model of ICNN C-transoform and MLP
-    when alpha=0, it is standard ICNN C-transform
-    """
-    temp_cp = None
-    # C_Transform ICNN
-    model = model_icnn.to(device)    
-    for ep in range(epoch):
-      for data in train_loader:
-          temp_data = data.float().to(device) # Training source
-          optimizer.zero_grad()
+  """
+  hybrid model of ICNN C-transoform and MLP
+  when alpha=0, it is standard ICNN C-transform
+  """
+  temp_cp = None
+  # C_Transform ICNN
+  model = model_icnn.to(device)    
+  for ep in range(epoch):
+    for data in train_loader:
+        temp_data = data.float().to(device) # Training source
+        optimizer.zero_grad()
 
-          fake_target = model.transport(temp_data)  # by grad of Phi_B(from icnn) 
-          if temp_cp == None:      
-            temp_cp = get_coupling(temp_data,model.target).to(device)
+        fake_target = model.transport(temp_data)  # by grad of Phi_B(from icnn) 
+        if temp_cp == None:      
+          temp_cp = get_coupling(temp_data,model.target).to(device)
 
 
-          loss1= (temp_cp* torch.cdist(fake_target,model.target)**2).sum()
+        loss1= (temp_cp* torch.cdist(fake_target,model.target)**2).sum()
 
-          optimizer.zero_grad()
-          loss2 = -model.dual(temp_data)
-          loss = alpha*loss1+(1-alpha)*loss2
+        optimizer.zero_grad()
+        loss2 = -model.dual(temp_data)
+        loss = alpha*loss1+(1-alpha)*loss2
 
-          loss.backward()
-          optimizer.step()
+        loss.backward()
+        optimizer.step()
 
-          for l in range(len(model.convex_func.w_zs)):
-             model.convex_func.w_zs[l].weight.data.copy_(torch.relu(model.convex_func.w_zs[l].weight.data))
+        for l in range(len(model.convex_func.w_zs)):
+           model.convex_func.w_zs[l].weight.data.copy_(torch.relu(model.convex_func.w_zs[l].weight.data))
 
-      if ep%100==0 and print_bool:
-        print(ep,loss1.data,-loss2.data)
-
+    if ep%100==0 and print_bool:
+      print(ep,loss1.data,-loss2.data)
